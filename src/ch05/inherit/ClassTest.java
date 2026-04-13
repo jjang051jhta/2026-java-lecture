1584ce6c533e1b1bcf37f0205cf589d17e1b4d57
jjang051 <tjjjang051@nate.com>
2026-04-13 13:18:33 +0900

    상속 02

---
+++ src/ch05/inherit/ClassTest.java
@@ -0,0 +1,9 @@
+package ch05.inherit;
+
+public class ClassTest {
+    static void main() {
+        Student student = new Student();
+        StudentWokrer studentWokrer = new StudentWokrer();
+        studentWokrer.work();
+    }
+}
