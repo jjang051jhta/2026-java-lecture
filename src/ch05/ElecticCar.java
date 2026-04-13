e33c84a1a8a00ceb2bd49ebd08ae5853cad9e173
jjang051 <tjjjang051@nate.com>
2026-04-13 12:07:23 +0900

    상속 override

--- src/ch05/ElecticCar.java
+++ src/ch05/ElecticCar.java
@@ -1,10 +1,18 @@
 package ch05;
 
 public class ElecticCar extends Car {
-//    void move() {
-//        System.out.println("차가 움직입니다.");
-//    }
+
+    //annotation
+    @Override
+    void move() {
+        System.out.println("전기차가 빠르게 움직입니다.");
+    }
+
     void charge() {
         System.out.println("충전합니다.");
     }
+    //override규칙
+    //1. 이름이 같아야 한다.
+    //2. 반환타입이 같아야 한다.
+    //3. 매개변수의 타입 , 갯수 , 순서가 같아야 한다.
 }
