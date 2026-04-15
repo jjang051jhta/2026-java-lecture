1e2d4164c7bcddc125383ece5e3765c9f0cc1123
jjang051 <tjjjang051@nate.com>
2026-04-15 16:49:55 +0900

    ch06 ocp pay system

--- src/ch06/pay/PayService.java
+++ src/ch06/pay/PayService.java
@@ -1,24 +1,23 @@
 package ch06.pay;
 
 public class PayService {
-    public void processPay(String option, int amount) {
-        boolean result;
-        System.out.println("결제를 시작합니다: option=" + option + ", amount=" +
-                amount);
-        if (option.equals("kakao")) {
+    public void processPay(String option,int amount) {
+        System.out.println("결제를 시도합니다. : option = "+option+" , amount = "+amount);
+        boolean result = false;
+        if(option.equals("kakao")) {
             KakaoPay kakaoPay = new KakaoPay();
             result = kakaoPay.pay(amount);
-        } else if (option.equals("naver")) {
+        } else if(option.equals("naver")) {
             NaverPay naverPay = new NaverPay();
             result = naverPay.pay(amount);
         } else {
-            System.out.println("결제 수단이 없습니다.");
+            System.out.println("결제수단이 없습니다.");
             result = false;
         }
-        if (result) {
+        if(result) {
             System.out.println("결제가 성공했습니다.");
         } else {
-            System.out.println("결제가 실패했습니다.");
+            System.out.println("결제에 실패했습니다.");
         }
     }
 }
--- src/ch06/pay/PayTest.java
+++ src/ch06/pay/PayTest.java
@@ -1,30 +1,18 @@
 package ch06.pay;
 
 public class PayTest {
-    public static void main(String[] args) {
+    static void main() {
         PayService payService = new PayService();
-        //kakao 결제
-        String payOption1 = "kakao";
-        int amount1 = 5000;
-        payService.processPay(payOption1, amount1);
-        //naver 결제
-        String payOption2 = "naver";
-        int amount2 = 10000;
-        payService.processPay(payOption2, amount2);
-        //잘못된 결제 수단 선택
-        String payOption3 = "bad";
-        int amount3 = 15000;
-        payService.processPay(payOption3, amount3);
+        String payOption01 = "kakao";
+        int amount01 = 100000;
+        payService.processPay(payOption01,amount01);
+
+        String payOption02 = "naver";
+        int amount02 = 150000;
+        payService.processPay(payOption02,amount02);
+
+        String payOption03 = "toss";
+        int amount03 = 250000;
+        payService.processPay(payOption03,amount03);
     }
 }
-//결제를 시작합니다: option=kakao, amount=5000
-//카카오페이 시스템과 연결합니다.
-//5000원 결제를 시도합니다.
-//결제가 성공했습니다.
-//결제를 시작합니다: option=naver, amount=10000
-//네이버페이 시스템과 연결합니다.
-//10000원 결제를 시도합니다.
-//결제가 성공했습니다.
-//결제를 시작합니다: option=bad, amount=15000
-//결제 수단이 없습니다.
-//결제가 실패했습니다.
