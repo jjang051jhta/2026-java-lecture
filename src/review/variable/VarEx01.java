d9683ca7e1f84d523d5080d4bbdfc677e35ba468
jjang051 <tjjjang051@nate.com>
2026-04-16 09:59:56 +0900

    review 변수

---
+++ src/review/variable/VarEx01.java
@@ -0,0 +1,10 @@
+package review.variable;
+
+public class VarEx01 {
+    static void main() {
+
+        System.out.println(4 + 3);
+        System.out.println(4 - 3);
+        System.out.println(4 * 3);
+    }
+}
