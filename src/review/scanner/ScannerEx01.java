b01f4d7220e8e39825481e754afcc64790be58cf
jjang051 <tjjjang051@nate.com>
2026-04-16 14:43:25 +0900

    review scanner

--- src/review/scanner/ScannerEx01.java
+++ src/review/scanner/ScannerEx01.java
@@ -5,12 +5,19 @@ import java.util.Scanner;
 public class ScannerEx01 {
     static void main() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("음식 이름을 입력해주세요 : ");
-        String menu = scanner.nextLine();
-        System.out.print("음식 가격을 입력해주세요 : ");
-        int price = scanner.nextInt();
-        System.out.print("음식 수량을 입력해주세요 : ");
-        int quantity = scanner.nextInt();
-        System.out.println(menu+quantity+"개를 주문하셨습니다. 총 가격은 "+(price*quantity)+"원입니다.");
+        while(true) {
+            System.out.print("음식 이름을 입력해주세요 exit를 누르면 종료합니다.: ");
+            String menu = scanner.nextLine(); //"라면 \n"
+            if(menu.equals("exit")) {
+                break;
+            }
+            System.out.print("음식 가격을 입력해주세요 : ");
+            int price = scanner.nextInt(); //1000
+            System.out.print("음식 수량을 입력해주세요 : ");
+            int quantity = scanner.nextInt(); //3
+            scanner.nextLine();
+            System.out.println(menu + quantity + "개를 주문하셨습니다. 총 가격은 " + (price * quantity) + "원입니다.");
+
+        }
     }
 }
