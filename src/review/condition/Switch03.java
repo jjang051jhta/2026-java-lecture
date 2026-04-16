f02c05685184208067652a20104728d917ebf3b9
jjang051 <tjjjang051@nate.com>
2026-04-16 11:38:02 +0900

    review switch

---
+++ src/review/condition/Switch03.java
@@ -0,0 +1,14 @@
+package review.condition;
+
+public class Switch03 {
+    static void main() {
+        int grade = 2;
+        int coupon = switch (grade) {
+            case 1 -> coupon = 1000;
+            case 2 -> coupon = 2000;
+            case 3 -> coupon = 3000;
+            default -> coupon = 500;
+        };
+        System.out.println("발급받은 쿠폰 : " + coupon);
+    }
+}
