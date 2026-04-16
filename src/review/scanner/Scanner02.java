25984e5b165c97fb520b6354bd87ff66a17cc284
jjang051 <tjjjang051@nate.com>
2026-04-16 14:07:14 +0900

    review scanner

--- src/review/scanner/Scanner02.java
+++ src/review/scanner/Scanner02.java
@@ -1,4 +1,17 @@
 package review.scanner;
 
+import java.util.Scanner;
+
 public class Scanner02 {
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("문자를 입력하세요. exit를 입력하면 종료합니다.");
+            String str = scanner.nextLine();
+            if(str.equals("exit")) {
+                break;
+            }
+            System.out.println(str);
+        }
+    }
 }
