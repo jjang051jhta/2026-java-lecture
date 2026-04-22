9ec665cb40081c6d630c3f368416d712fc8d62c0
jjang051 <tjjjang051@nate.com>
2026-04-22 13:08:26 +0900

    ch07 enumeration ex02

---
+++ src/ch08/enumeration/ex04/Grade.java
@@ -0,0 +1,13 @@
+package ch08.enumeration.ex04;
+
+import ch08.enumeration.ex03.ClassGrade;
+
+public enum Grade {
+    BASIC,GOLD,DIAMOND
+
+//    public static final ClassGrade BASIC = new ClassGrade();
+//    public static final ClassGrade GOLD = new ClassGrade();
+//    public static final ClassGrade DIAMOND =new ClassGrade();
+//    private ClassGrade() {
+//    }
+}
