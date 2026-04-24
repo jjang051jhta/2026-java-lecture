d57c90014c82de1e2c74f1b35c4eab36c3e0af59
jjang051 <tjjjang051@nate.com>
2026-04-24 15:55:09 +0900

    ch09 예외처리 02_02

--- src/ch10/exception/ex02/Main02.java
+++ src/ch10/exception/ex02/Main02.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class Main02 {
     static void main() {
-        NetworkService02 networkService02 = new NetworkService02();
+        //NetworkService02 networkService = new NetworkService02();
+        NetworkService02_02 networkService = new NetworkService02_02();
         Scanner scanner = new Scanner(System.in);
         while(true) {
             System.out.print("전송할 문자 : ");
@@ -12,7 +13,7 @@ public class Main02 {
             if(input.equals("exit")) {
                 break;
             }
-            networkService02.sendMessage(input);
+            networkService.sendMessage(input);
             System.out.println();
         }
         System.out.println("프로그램을 종료합니다.");
--- src/ch10/exception/ex02/NetworkService02.java
+++ src/ch10/exception/ex02/NetworkService02.java
@@ -5,7 +5,7 @@ public class NetworkService02 {
         String address = "http://example.com";
         NetworkClient02 networkClient02 = new NetworkClient02(address);
         networkClient02.initError(data);
-        networkClient02.connect();
+        networkClient02.connect(); //success
         networkClient02.send(data);
         networkClient02.disconnect();
     }
