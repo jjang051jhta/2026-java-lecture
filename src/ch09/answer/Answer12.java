e5431bccea02c62b475ad8341ea532ddfd455109
jjang051 <tjjjang051@nate.com>
2026-04-24 13:15:36 +0900

    ch08 time with

--- src/ch09/answer/Answer12.java
+++ src/ch09/answer/Answer12.java
@@ -9,6 +9,7 @@ public class Answer12 {
     static void main() {
         LocalDateTime start = LocalDateTime.of(2026,4,24,22,30);
         LocalDateTime end = LocalDateTime.of(2026,4,25,6,10);
+        // 금일 22 : 30 ~ 내일 6 : 10
         Duration duration = Duration.between(start,end);
         System.out.println(duration.toHours()+"시간 "+duration.toMinutesPart()+"분");
     }
