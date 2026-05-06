890f2c27e16907b3f11932b5e6e7c8117f678aba
jjang051 <tjjjang051@nate.com>
2026-05-06 15:41:04 +0900

    ch11 file path

--- src/ch12/files/answer/Answer01.java
+++ src/ch12/files/answer/Answer01.java
@@ -12,11 +12,13 @@ public class Answer01 {
     static void main() throws IOException {
         Path uploadFile = Path.of("temp/cat.jpg");
         String fileName = uploadFile.getFileName().toString();
+        String name = fileName.substring(0,fileName.lastIndexOf("."));
         String ext = fileName.substring(fileName.lastIndexOf("."));
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
         String newName = LocalDateTime.now().format(formatter);
         //resolve()는 자바가 운영체제에 따라서 경로를 해결해 준다.
-        Path target =  uploadFile.getParent().resolve(newName+ext);
+        Path target =  uploadFile.getParent().resolve(name+"_"+newName+ext);
+        //cat_202605061533333.jpg
         //C:\Users\tjoeunis205\Downloads
         //Path target =  Paths.get("temp/"+newName+ext); //
         Files.move(uploadFile,target);
