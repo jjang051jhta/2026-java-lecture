351c1750f20a5ff84c01d765e796f61696afec71
jjang051 <tjjjang051@nate.com>
2026-05-07 11:21:15 +0900

    ch11 file path

--- src/ch13/thread/start/HelloRunnableMain.java
+++ src/ch13/thread/start/HelloRunnableMain.java
@@ -1,11 +1,15 @@
 package ch13.thread.start;
 
+import ch13.utils.MyLogger;
+
 public class HelloRunnableMain {
     static void main() {
-        System.out.println(Thread.currentThread().getName()+ " : main() start");
+        //System.out.println(Thread.currentThread().getName()+ " : main() start");
+        MyLogger.log("main() start");
         HelloRunnable helloRunnable = new HelloRunnable();
         Thread thread = new Thread(helloRunnable);
         thread.start();
-        System.out.println(Thread.currentThread().getName()+ " : main() end");
+        //System.out.println(Thread.currentThread().getName()+ " : main() end");
+        MyLogger.log("main() end");
     }
 }
