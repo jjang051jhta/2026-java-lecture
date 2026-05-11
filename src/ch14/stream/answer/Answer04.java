05eae036983173fe48002ec0e06e5b89692b04cd
jjang051 <tjjjang051@nate.com>
2026-05-11 10:00:11 +0900

    ch14 stream answer

--- src/ch14/stream/answer/Answer04.java
+++ src/ch14/stream/answer/Answer04.java
@@ -1,9 +1,8 @@
 package ch14.stream.answer;
 
 import java.util.List;
-import java.util.function.Function;
 
-public class Amswer04 {
+public class Answer04 {
     static void main() {
         List<Integer> nums = List.of(1, 2, 3, 4, 5);
         List<Integer> square = nums.stream().map(n -> n * n).toList();
